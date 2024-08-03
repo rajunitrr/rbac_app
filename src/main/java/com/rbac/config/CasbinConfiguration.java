06364c4448415bdda5f11f256182a8ee726b2859
PraveenG <praveenkumarg@ivycomptech.com>
2024-08-03 18:25:50 +0530

    Loading the policies on startup of the service

--- src/main/java/com/rbac/config/CasbinConfiguration.java
+++ src/main/java/com/rbac/config/CasbinConfiguration.java
@@ -1,28 +1,48 @@
 package com.rbac.config;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.casbin.jcasbin.main.Enforcer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import com.rbac.entity.Policy;
+import com.rbac.service.PolicyService;
+
 @Configuration
 public class CasbinConfiguration {
+
+	@Autowired
+	private PolicyService policyService;
+
+	Enforcer enforcer;
 
 	@Bean
 	public Enforcer casbinEnforcer() {
 		String modelPath = null;
-		String policyPath = null;		
+		String policyPath = null;
 		try {
 			modelPath = new ClassPathResource("acs.conf").getFile().getAbsolutePath();
-			policyPath = new ClassPathResource("casbin_policy.csv").getFile().getAbsolutePath();
+			// policyPath = new ClassPathResource("policy.csv").getFile().getAbsolutePath();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		enforcer = new Enforcer(modelPath);
+		loadPolicies();
+		return enforcer;
+	}
 
-		return new Enforcer(modelPath, policyPath);
+	public void loadPolicies() {
+		List<Policy> policies = policyService.getAllPolicies();
+		for (Policy policy : policies) {
+			addPolicy(policy.getSub(), policy.getDom(), policy.getObj(), policy.getAct(), policy.getEft());
+		}
+	}
 
+	public boolean addPolicy(String sub, String dom, String obj, String act, String eft) {
+		return enforcer.addPolicy(sub, dom, obj, act, eft);
 	}
 }
